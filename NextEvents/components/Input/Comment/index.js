b8f455c67807e840b0845d77d7e5ede59b2d6f6a
filliperomero <fillipe.romero@gmail.com>
2021-03-23 20:45:52 -0300

    chore: add comments component

---
+++ NextEvents/components/Input/Comment/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import CommentList from '../CommentList';
+import NewComment from '../NewComment';
+
+import classes from './styles.module.css';
+
+const Comments = ({ eventId }) => {
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? 'Hide' : 'Show'} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+}
+
+export default Comments;
