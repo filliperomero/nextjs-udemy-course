37c14c10499acc3c0fee27ca65c66490f288dea2
filliperomero <fillipe.romero@gmail.com>
2021-03-25 23:29:41 -0300

    chore: add notification to comments component

--- NextEvents/components/Input/Comments/index.js
+++ NextEvents/components/Input/Comments/index.js
@@ -1,23 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
 import CommentList from '../CommentList';
 import NewComment from '../NewComment';
+import NotificationContext from '../../../context/NotificationContext'
 
 import classes from './styles.module.css';
 
 const Comments = ({ eventId }) => {
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { showNotification } = useContext(NotificationContext);
 
   useEffect(() => {
     if (showComments) {
-        axios.get(`/api/comments/${eventId}`)
-          .then(response => {
-            console.log(response.data)
-            setComments(response.data.comments)
-          })
-          .catch(error => console.log(error))
+      setLoading(true);
+      axios.get(`/api/comments/${eventId}`)
+        .then(response => {
+          setComments(response.data.comments)
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+          setLoading(false)
+        })
     }
   }, [showComments])
 
@@ -26,10 +32,25 @@ const Comments = ({ eventId }) => {
   }
 
   const handleAddComment = async (commentData) => {
+    showNotification({
+      title: 'Sending comment...',
+      message: 'Your comment is currently being stored into a database',
+      status: 'pending'
+    })
     try {
       const { data } = await axios.post(`/api/comments/${eventId}`, commentData)
+      setComments(state => [...state, data.comment])
+      showNotification({
+        title: 'Success',
+        message: 'Your comment was saved',
+        status: 'success'
+      })
     } catch (error) {
-      console.log(error.response.data)
+      showNotification({
+        title: 'Error',
+        message: error.response.data.message ? error.response.data.message : 'Something went wrong!',
+        status: 'error'
+      })
     }
   }
 
@@ -39,7 +60,8 @@ const Comments = ({ eventId }) => {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={handleAddComment} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !loading && <CommentList items={comments} />}
+      {showComments && loading && <p>Loading...</p>}
     </section>
   );
 }
