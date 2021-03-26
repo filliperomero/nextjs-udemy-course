234e3b7c419f6cc8b0ec8512682ac0375420f828
filliperomero <fillipe.romero@gmail.com>
2021-03-25 23:28:08 -0300

    chore: add notification to our layout

--- NextEvents/components/Layout/index.js
+++ NextEvents/components/Layout/index.js
@@ -1,12 +1,25 @@
+import { useContext } from 'react'
+
 import Header from './Header'
+import Notification from '../Ui/Notification'
+import NotificationContext from '../../context/NotificationContext'
 
 const Layout = ({ children }) => {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <Header />
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
+      {activeNotification && (
+        <Notification 
+          title={activeNotification.title} 
+          message={activeNotification.message} 
+          status={activeNotification.status} 
+        />
+      )}
     </>
   )
 }
