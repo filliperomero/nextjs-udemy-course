9db8fff24999bc725d065ac886ff9215c57ea94f
filliperomero <fillipe.romero@gmail.com>
2021-03-27 00:48:30 -0300

    chore: rename project and add Logo/MainNavigation component

---
+++ NextBlog/components/Layout/Logo/index.js
@@ -0,0 +1,11 @@
+import classes from './styles.module.css';
+
+const Logo = () => {
+  return (
+    <div className={classes.logo}>
+      Fillipe's Next Blog
+    </div>
+  )
+}
+
+export default Logo;
