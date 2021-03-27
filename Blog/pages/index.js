a56a1dfbc729b3813f21687c390f700a67a0bc96
filliperomero <fillipe.romero@gmail.com>
2021-03-26 23:21:11 -0300

    chore: add basic pages

---
+++ Blog/pages/index.js
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero';
+import FeaturedPosts from '../components/FeaturedPosts';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  )
+}
+
+export default Home;
