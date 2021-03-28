8c23c75267dffb176a8ac76e1884894623e37e14
filliperomero <fillipe.romero@gmail.com>
2021-03-28 00:49:37 -0300

    chore: add real posts to index

--- NextBlog/pages/index.js
+++ NextBlog/pages/index.js
@@ -1,44 +1,25 @@
 import Hero from '../components/Hero';
 import FeaturedPosts from '../components/FeaturedPosts';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  }
-]
+import { getFeaturedPosts } from '../lib/postsUtil' 
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const featuredPosts = await getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts
+    }
+  }
 }
 
 export default Home;
