2058c4166370bc17beafb754c8049d9d11464bbb
filliperomero <fillipe.romero@gmail.com>
2021-03-28 00:50:04 -0300

    chore: add real posts to postsPage

--- NextBlog/pages/posts/index.js
+++ NextBlog/pages/posts/index.js
@@ -1,38 +1,19 @@
 import AllPosts from '../../components/Posts/AllPosts';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is a React framework for production.',
-    date: '2022-02-10'
-  }
-]
+import { getAllposts } from '../../lib/postsUtil';
 
-const Posts = () => {
-  return <AllPosts posts={DUMMY_POSTS} />
+const Posts = ({ posts }) => {
+  return <AllPosts posts={posts} />
+}
+
+export const getStaticProps = async () => {
+  const allPosts = await getAllposts();
+
+  return {
+    props: {
+      posts: allPosts
+    }
+  }
 }
 
 export default Posts;
