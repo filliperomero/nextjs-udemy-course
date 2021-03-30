e49f4ddd82dbb780b0a4a6cd728055a6c08997dc
filliperomero <fillipe.romero@gmail.com>
2021-03-29 22:09:18 -0300

    chore: add SEO to index page

--- NextBlog/pages/posts/index.js
+++ NextBlog/pages/posts/index.js
@@ -1,9 +1,19 @@
+import Head from 'next/head'
+
 import AllPosts from '../../components/Posts/AllPosts';
 
 import { getAllposts } from '../../lib/postsUtil';
 
 const Posts = ({ posts }) => {
-  return <AllPosts posts={posts} />
+  return (
+    <>
+      <Head>
+        <title>All Posts</title>
+        <meta name="description" content="A list of all programming-related tutorials and posts!" />
+      </Head>
+      <AllPosts posts={posts} />
+    </>
+  )
 }
 
 export const getStaticProps = async () => {
