388ef60ebf972de9dd6e75779d2d530fb3c02683
filliperomero <fillipe.romero@gmail.com>
2021-03-29 22:09:28 -0300

    chore: add SEO to post detail page

--- NextBlog/pages/posts/[slug].js
+++ NextBlog/pages/posts/[slug].js
@@ -1,8 +1,18 @@
+import Head from 'next/head'
+
 import PostContent from '../../components/Posts/PostDetail/PostContent';
 import { getPostData, getPostsFiles } from '../../lib/postsUtil'
 
 const PostDetail = ({ post }) => {
-  return <PostContent post={post} />;
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
+      </Head>
+      <PostContent post={post} />
+    </>
+  )
 }
 
 export const getStaticProps = async (context) => {
