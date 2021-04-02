752a54213c7efd61918739b5327e8fe3bbcd5405
filliperomero <fillipe.romero@gmail.com>
2021-04-02 17:10:57 -0300

    chore: add calls to the api when signing in a user

--- NextAuth/components/Auth/AuthForm/index.js
+++ NextAuth/components/Auth/AuthForm/index.js
@@ -1,25 +1,75 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router'
 
 import classes from './styles.module.css';
 
+const createUser = async (email, password) => {
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong')
+  }
+
+  return data;
+}
+
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const router = useRouter();
 
   const handleSwitchAuth = () => {
     setIsLogin((prevState) => !prevState);
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const email = emailInputRef.current.value
+    const password = passwordInputRef.current.value
+
+    if (isLogin) {
+      const result = await signIn('credentials', { redirect: false, email, password })
+      if (!result.error) {
+        return router.replace('/profile')
+      }
+      console.log(result)
+      return;
+    }
+
+    // TODO: Add validation
+    try {
+      const user = await createUser(email, password)
+      console.log(user)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
