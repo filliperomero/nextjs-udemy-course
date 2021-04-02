052e766fec98d618b4d25223a8f3d6d0993343e6
filliperomero <fillipe.romero@gmail.com>
2021-04-02 17:10:14 -0300

    chore: add session to auth page to redirect when necessary

--- NextAuth/pages/auth.js
+++ NextAuth/pages/auth.js
@@ -1,7 +1,26 @@
+import { getSession } from 'next-auth/client'
+
 import AuthForm from '../components/Auth/AuthForm';
 
 const AuthPage = () => {
   return <AuthForm />;
+}
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req })
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: { session }
+  }
 }
 
 export default AuthPage;
