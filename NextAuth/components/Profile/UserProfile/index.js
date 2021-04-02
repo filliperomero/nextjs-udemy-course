72807f46320edf0506253dc2f2f433ce105446cc
filliperomero <fillipe.romero@gmail.com>
2021-04-02 17:12:24 -0300

    chore: add call to the api when updating profile

--- NextAuth/components/Profile/UserProfile/index.js
+++ NextAuth/components/Profile/UserProfile/index.js
@@ -3,12 +3,41 @@ import ProfileForm from '../ProfileForm';
 import classes from './styles.module.css';
 
 const UserProfile = () => {
-  // Redirect away if NOT auth
+  // Less elegant way to check if the page should show (if the user has a valid session)
+  // const [isLoading, setIsloading] = useState(true);
+
+  // useEffect(() => {
+  //   getSession().then(session => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsloading(false)
+  //     }
+  //   })
+  // }, [getSession])
+
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
+
+  const handleChangePassword = async (passwordData) => {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(passwordData)
+    })
+
+    const data = await response.json();
+
+    console.log(data);
+  }
 
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={handleChangePassword} />
     </section>
   );
 }
