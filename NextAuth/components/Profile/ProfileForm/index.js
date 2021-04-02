bf545ebdd63cbb32c0337f96174e26d222454640
filliperomero <fillipe.romero@gmail.com>
2021-04-02 17:12:48 -0300

    chore: add logic to call the correct function when user wants to change password

--- NextAuth/components/Profile/ProfileForm/index.js
+++ NextAuth/components/Profile/ProfileForm/index.js
@@ -1,18 +1,37 @@
+import { useRef } from 'react'
+
 import classes from './styles.module.css';
 
-const ProfileForm = () => {
+const ProfileForm = ({ onChangePassword }) => {
+  const oldPasswordInputRef = useRef();
+  const newPasswordInputRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const oldPassword = oldPasswordInputRef.current.value
+    const newPassword = newPasswordInputRef.current.value
+
+    // TODO: add validation
+    
+    onChangePassword({
+      oldPassword,
+      newPassword
+    })
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input type='password' id='old-password' ref={oldPasswordInputRef} />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button button="submit">Change Password</button>
       </div>
     </form>
   );
