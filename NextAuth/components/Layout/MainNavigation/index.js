c57f1c261d6c5d360be6196d6e27783214683d76
filliperomero <fillipe.romero@gmail.com>
2021-04-02 17:12:03 -0300

    chore: add signOut ability and validations to show the correct navigation when authenticated

--- NextAuth/components/Layout/MainNavigation/index.js
+++ NextAuth/components/Layout/MainNavigation/index.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/client'
 
 import classes from './styles.module.css';
 
 const MainNavigation = () => {
+  const [session, loading] = useSession();
+
+  const handleLogout = () => {
+    signOut();
+  }
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -12,15 +19,21 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button type="button">Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+          {session && (
+            <>
+              <li>
+                <Link href='/profile'>Profile</Link>
+              </li>
+              <li>
+                <button type="button" onClick={handleLogout}>Logout</button>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
